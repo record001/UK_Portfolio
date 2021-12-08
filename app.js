6f2a00460418a18335f7ff0c7af66ce81a208f6f
record001 <umarkhidirboev@gmail.com>
2021-12-08 10:52:37 +0500

    burger

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let nav = document.querySelector(".nav");
 let modal = document.querySelector(".modal");
 let up = document.querySelector("#up");
-
+let modal_link = document.querySelectorAll(".modal__link")
+// console.log(modal_link);
+let burger = document.querySelector(".burger")
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 20) {
@@ -20,3 +22,9 @@ function myFunction(x) {
   modal.classList.toggle("active");
 }
 
+modal_link.forEach(link => {
+  link.addEventListener("click", ()=>{
+    myFunction(burger)
+    modal.classList.remove("active")
+  })
+})
